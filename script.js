1d3cecb97b8ebfd2230c56d7e6b227863aa3314d
Trang Ngo <tranghthngo@gmail.com>
2021-07-18 12:01:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+
+    result.innerText = `If you deposit ${principle},\nat an interest rate of ${rate}%.\nYou will
+    receive an amount of ${principle + interest},\nin the year ${year}`;
 }
-        
+
+function updateRate()
+{
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateVal;
+}
