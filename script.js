7b16f3af7c84a76bab999a79faf9c7d0b10ce502
Trang Ngo <tranghthngo@gmail.com>
2021-07-18 12:31:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
